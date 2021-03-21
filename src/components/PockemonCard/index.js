37219aebf7556a83fbc96722ac3211c33cdc9683
nikavolyagoalie <nikolasmad@yandex.ru>
2021-03-21 15:07:04 +0300

    M

--- src/components/PockemonCard/index.js
+++ src/components/PockemonCard/index.js
@@ -3,11 +3,10 @@ import s from'./style.module.css'
 import cardBackSide from './assets/card-back-side.jpg'
 import cn from 'classnames';
 
-const PockemonCard = ({name, img, id, type, values}) => {
-    const [isActive, setActive] = useState(false)
+const PockemonCard = ({name, img, id, type, values, clickCard, isActive}) => {
 
     const handler = () => {
-        setActive(!isActive)
+        clickCard && clickCard(id);
     }
     return (
         <div className={cn(s.root)} onClick={handler}>
